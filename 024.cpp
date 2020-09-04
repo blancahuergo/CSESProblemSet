a10b24d5718828832dd6fa2a7717f6774ca1941e
blancahuergo <44428598+blancahuergo@users.noreply.github.com>
2020-09-04 17:18:57 +0200

    Create 024.cpp

---
+++ 024.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <cfloat>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <cstring>
+#include <cmath>
+#include <climits>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+typedef pair<int, int> pii;
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef vector<vvvi> vvvvi;
+typedef vector<pii> vpii;
+typedef vector<vpii> vvpii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<vvl> vvvl;
+typedef vector<vvvl> vvvvl;
+typedef vector<string> vs;
+#define f first
+#define s second
+#define pb push_back
+#define eb emplace_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define mp make_pair
+#define len(v) ((int)v.size())
+#define all(v) v.begin(), v.end()
+ 
+ 
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll rec = INT_MIN, cur = 0, N, num;
+	cin >> N;
+	while(N--) {
+		cin >> num;
+		cur += num;
+		rec = max(rec, cur);
+		cur = max(cur, 0ll);
+	}
+	cout << rec;
+	return 0;
+}
